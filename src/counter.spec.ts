c8b21e2a09349166b3d722de9fba3fb62f8671bf
Yonatan Kra <yonatan.kra@vonage.com>
2023-09-07 14:16:38 +0300

    Vitest is working

---
+++ src/counter.spec.ts
@@ -0,0 +1,10 @@
+import { setupCounter } from "./counter";
+
+
+describe('counter', () => {
+    describe('setupCounter', function () {
+        it('should be defined', function () {
+            expect(setupCounter).toBeDefined();
+        });
+    });
+});
